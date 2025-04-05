f3fc98f664165398110e26b444fc94b8d147e81f
dearone256 <deardeerone@gmail.com>
2025-04-05 19:08:19 +0300

    ввожу параметры в методы

--- test_add_group.py
+++ test_add_group.py
@@ -20,31 +20,31 @@ class test_add_group (unittest.TestCase):
 	def Open_main_page(self, nm):
 		nm.get("http://localhost/addressbook/")
 
-	def Login(self, wd):
+	def Login(self, wd, username, password):
 		wd.find_element(By.NAME, "user").click()
 		wd.find_element(By.NAME, "user").clear()
-		wd.find_element(By.NAME, "user").send_keys("admin")
+		wd.find_element(By.NAME, "user").send_keys(username)
 		wd.find_element(By.NAME, "pass").click()
 		wd.find_element(By.NAME, "pass").clear()
-		wd.find_element(By.NAME, "pass").send_keys("secret")
+		wd.find_element(By.NAME, "pass").send_keys(password)
 		wd.find_element(By.CSS_SELECTOR, "input[type=\"submit\"]").click()
 
 	def Open_group_page(self, pp):
 		pp.find_element(By.LINK_TEXT, "groups").click()
 
-	def Create_group(self, wd):
+	def Create_group(self, wd, name, header, footer):
 		# init new group
 		wd.find_element(By.NAME, "new").click()
 		# fill group form
 		wd.find_element(By.NAME, "group_name").click()
 		wd.find_element(By.NAME, "group_name").clear()
-		wd.find_element(By.NAME, "group_name").send_keys("dfgdfg")
+		wd.find_element(By.NAME, "group_name").send_keys(name)
 		wd.find_element(By.NAME, "group_header").click()
 		wd.find_element(By.NAME, "group_header").clear()
-		wd.find_element(By.NAME, "group_header").send_keys("dfgdfg")
+		wd.find_element(By.NAME, "group_header").send_keys(header)
 		wd.find_element(By.NAME, "group_footer").click()
 		wd.find_element(By.NAME, "group_footer").clear()
-		wd.find_element(By.NAME, "group_footer").send_keys("dfgfghgfhg")
+		wd.find_element(By.NAME, "group_footer").send_keys(footer)
 		# submit group creation
 		wd.find_element(By.NAME, "submit").click()
 
@@ -54,12 +54,21 @@ class test_add_group (unittest.TestCase):
 	def Logout(self, wd):
 		wd.find_element(By.LINK_TEXT, "Logout").click()
 
-	def test_test_add_group (self):
+	def test_add_group (self):
 		wd = self.wd
 		self.Open_main_page(wd)
-		self.Login(wd)
+		self.Login(wd, username="admin", password="secret")
 		self.Open_group_page(wd)
-		self.Create_group(wd)
+		self.Create_group(wd, name="dfgdfg451", header="dfgdfg452", footer="dfgfghgfhg453")
+		self.Return_to_groups_page(wd)
+		self.Logout(wd)
+
+	def test_add_empty_group (self):
+		wd = self.wd
+		self.Open_main_page(wd)
+		self.Login(wd, username="admin", password="secret")
+		self.Open_group_page(wd)
+		self.Create_group(wd, name="", header="", footer="")
 		self.Return_to_groups_page(wd)
 		self.Logout(wd)
 
