9b90705afccb5c6199fba06fed489a3dd766d2a8
dearone256 <deardeerone@gmail.com>
2025-04-05 17:26:43 +0300

    Добавил коммент и выделил отдельные методы

--- test_add_group.py
+++ test_add_group.py
@@ -17,19 +17,25 @@ class test_add_group (unittest.TestCase):
 		self.wd = WebDriver()
 		self.wd.implicitly_wait (60)
 
-	def test_test_add_group (self):
-		success = True
-		wd = self.wd
-		wd.get("http://localhost/addressbook/")
+	def Open_main_page(self, nm):
+		nm.get("http://localhost/addressbook/")
+
+	def Login(self, wd):
 		wd.find_element(By.NAME, "user").click()
 		wd.find_element(By.NAME, "user").clear()
 		wd.find_element(By.NAME, "user").send_keys("admin")
 		wd.find_element(By.NAME, "pass").click()
 		wd.find_element(By.NAME, "pass").clear()
-		wd.find_element(By.NAME, "pass").send_keys ("secret")
+		wd.find_element(By.NAME, "pass").send_keys("secret")
 		wd.find_element(By.CSS_SELECTOR, "input[type=\"submit\"]").click()
-		wd.find_element(By.LINK_TEXT, "groups").click()
+
+	def Open_group_page(self, pp):
+		pp.find_element(By.LINK_TEXT, "groups").click()
+
+	def Create_group(self, wd):
+		# init new group
 		wd.find_element(By.NAME, "new").click()
+		# fill group form
 		wd.find_element(By.NAME, "group_name").click()
 		wd.find_element(By.NAME, "group_name").clear()
 		wd.find_element(By.NAME, "group_name").send_keys("dfgdfg")
@@ -39,10 +45,23 @@ class test_add_group (unittest.TestCase):
 		wd.find_element(By.NAME, "group_footer").click()
 		wd.find_element(By.NAME, "group_footer").clear()
 		wd.find_element(By.NAME, "group_footer").send_keys("dfgfghgfhg")
+		# submit group creation
 		wd.find_element(By.NAME, "submit").click()
+
+	def Return_to_groups_page(self, wd):
 		wd.find_element(By.LINK_TEXT, "group page").click()
+
+	def Logout(self, wd):
 		wd.find_element(By.LINK_TEXT, "Logout").click()
-		self.assertTrue (success)
+
+	def test_test_add_group (self):
+		wd = self.wd
+		self.Open_main_page(wd)
+		self.Login(wd)
+		self.Open_group_page(wd)
+		self.Create_group(wd)
+		self.Return_to_groups_page(wd)
+		self.Logout(wd)
 
 	def tearDown(self):
 		self.wd.quit()
--- test_create_group1.py
+++ test_create_group1.py
@@ -1,31 +1,68 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 
-class TestTestcreategroup1():
+class Testcreategroup1():
   def setup_method(self, method):
     self.driver = webdriver.Firefox()
     self.vars = {}
   
   def teardown_method(self, method):
     self.driver.quit()
-  
-  def test_testcreategroup1(self):
+
+  def open_home_page(self):
     self.driver.get("http://localhost/addressbook/")
+
+  def resize_window(self):
     self.driver.set_window_size(1936, 1056)
+
+  def login(self):
     self.driver.find_element(By.NAME, "user").send_keys("admin")
     self.driver.find_element(By.NAME, "pass").send_keys("secret")
     self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(7)").click()
+
+  def open_group_page(self):
     self.driver.find_element(By.LINK_TEXT, "groups").click()
+
+  def create_group(self):
+    # init new group
     self.driver.find_element(By.NAME, "new").click()
+    # fill group form
     self.driver.find_element(By.NAME, "group_name").click()
-    self.driver.find_element(By.NAME, "group_name").send_keys("grname_01")
+    self.driver.find_element(By.NAME, "group_name").send_keys("grname_02")
     self.driver.find_element(By.NAME, "group_header").click()
-    self.driver.find_element(By.NAME, "group_header").send_keys("grheader_01")
+    self.driver.find_element(By.NAME, "group_header").send_keys("grheader_02")
     self.driver.find_element(By.NAME, "group_footer").click()
-    self.driver.find_element(By.NAME, "group_footer").send_keys("grfooter_01")
+    self.driver.find_element(By.NAME, "group_footer").send_keys("grfooter_02")
+    # submit group creation
     self.driver.find_element(By.NAME, "submit").click()
+
+  def return_to_group_page(self):
     self.driver.find_element(By.LINK_TEXT, "group page").click()
+
+  def logout(self):
     self.driver.find_element(By.LINK_TEXT, "Logout").click()
+
+  def test_testcreategroup1(self):
+    self.open_home_page()
+    self.resize_window()
+    self.login()
+    self.open_group_page()
+    self.create_group()
+    self.return_to_group_page()
+    self.logout()
+
+
+
+
+
+
+
+
+
+
+
+
   
