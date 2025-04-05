fa02e8606d62b3ee5bca87cd6fcce42390744aa7
dearone256 <deardeerone@gmail.com>
2025-04-05 19:08:45 +0300

    ввожу параметры в методы, тут начинается  предупреждение 'LF' to 'CRLF'

--- test_create_group1.py
+++ test_create_group1.py
@@ -1,10 +1,8 @@
-
+# -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 
-
-class Testcreategroup1():
+class Test_create_group1():
   def setup_method(self, method):
     self.driver = webdriver.Firefox()
     self.vars = {}
@@ -18,24 +16,24 @@ class Testcreategroup1():
   def resize_window(self):
     self.driver.set_window_size(1936, 1056)
 
-  def login(self):
-    self.driver.find_element(By.NAME, "user").send_keys("admin")
-    self.driver.find_element(By.NAME, "pass").send_keys("secret")
+  def login(self, username, password):
+    self.driver.find_element(By.NAME, "user").send_keys(username)
+    self.driver.find_element(By.NAME, "pass").send_keys(password)
     self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(7)").click()
 
   def open_group_page(self):
     self.driver.find_element(By.LINK_TEXT, "groups").click()
 
-  def create_group(self):
+  def create_group(self, name, header, footer):
     # init new group
     self.driver.find_element(By.NAME, "new").click()
     # fill group form
     self.driver.find_element(By.NAME, "group_name").click()
-    self.driver.find_element(By.NAME, "group_name").send_keys("grname_02")
+    self.driver.find_element(By.NAME, "group_name").send_keys(name)
     self.driver.find_element(By.NAME, "group_header").click()
-    self.driver.find_element(By.NAME, "group_header").send_keys("grheader_02")
+    self.driver.find_element(By.NAME, "group_header").send_keys(header)
     self.driver.find_element(By.NAME, "group_footer").click()
-    self.driver.find_element(By.NAME, "group_footer").send_keys("grfooter_02")
+    self.driver.find_element(By.NAME, "group_footer").send_keys(footer)
     # submit group creation
     self.driver.find_element(By.NAME, "submit").click()
 
@@ -45,15 +43,23 @@ class Testcreategroup1():
   def logout(self):
     self.driver.find_element(By.LINK_TEXT, "Logout").click()
 
-  def test_testcreategroup1(self):
+  def test_create_group1(self):
     self.open_home_page()
     self.resize_window()
-    self.login()
+    self.login(username="admin" , password="secret")
     self.open_group_page()
-    self.create_group()
+    self.create_group(name="grname_03", header="grheader_03", footer="grfooter_03")
     self.return_to_group_page()
     self.logout()
 
+  def test_create_empty_group1(self):
+    self.open_home_page()
+    self.resize_window()
+    self.login(username="admin" , password="secret")
+    self.open_group_page()
+    self.create_group(name="", header="", footer="")
+    self.return_to_group_page()
+    self.logout()
 
 
 
