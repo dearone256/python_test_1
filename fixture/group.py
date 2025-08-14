fec811ef5240154f27caa87a50c60fab7a2bdcde
dearone256 <deardeerone@gmail.com>
2025-08-15 00:07:22 +0300

    Сделал проверку при открытии страницы группы. Не открывать если уже открыта

--- fixture/group.py
+++ fixture/group.py
@@ -7,7 +7,8 @@ class GroupHelper:
 
     def Open_group_page(self):
         wd = self.app.wd
-        wd.find_element(By.LINK_TEXT, "groups").click()
+        if not (wd.current_url.endswith("group.php") and len(wd.find_elements(By.NAME, "new"))>0):
+            wd.find_element(By.LINK_TEXT, "groups").click()
 
     def count(self):
         wd = self.app.wd
